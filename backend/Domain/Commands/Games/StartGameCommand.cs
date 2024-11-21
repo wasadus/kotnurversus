09d2f8737595a9c9507acfae7aa78303dab0d75b
cmbbigis <g0r1lla.disk@gmail.com>
2024-11-21 20:47:32 +0500

    ReconstructionOfTournament: выпилил старые методы

--- backend/Domain/Commands/Games/StartGameCommand.cs
+++ backend/Domain/Commands/Games/StartGameCommand.cs
@@ -1,5 +1,4 @@
 using Domain.Context;
-using Domain.Helpers;
 using Domain.Services.Games;
 using Domain.Services.Rounds;
 using Microsoft.AspNetCore.JsonPatch;
@@ -25,40 +24,6 @@ public class StartGameCommand : IStartGameCommand
         this.dataContextAccessor = dataContextAccessor;
         this.gamesService = gamesService;
         this.roundsService = roundsService;
-    }
-
-    public async Task<DomainResult<Game, InvalidGameDataReason>> RunAsync(Guid id, List<RoundCreationArgs> roundsToCreate)
-    {
-        var result = await dataContextAccessor.AccessDataAsync<DomainResult<Game, InvalidGameDataReason>>(
-            async dbContext =>
-            {
-                var game = await gamesService.FindAsync(id);
-                if (game == null)
-                    return new ErrorInfo<InvalidGameDataReason>(InvalidGameDataReason.GameNotFound, "Game not found");
-
-                if (!IsTreeCorrect(game, roundsToCreate))
-                    return new ErrorInfo<InvalidGameDataReason>(InvalidGameDataReason.InvalidData, "Tree is not valid");
-
-                await CreateAllTree(roundsToCreate);
-                await gamesService.PatchAsync(
-                    game,
-                    new JsonPatchDocument<Game>(
-                        new List<Operation<Game>>()
-                        {
-                            new()
-                            {
-                                op = "replace",
-                                path = "/state",
-                                value = GameState.InProgress
-                            }
-                        },
-                        new DefaultContractResolver()));
-
-                await dbContext.SaveChangesAsync();
-                return game;
-            });
-
-        return result;
     }
 
     public async Task<DomainResult<Game, InvalidGameDataReason>> RunAsync(Guid id, StartGameRequest parameters)
@@ -371,64 +336,6 @@ public class StartGameCommand : IStartGameCommand
             && parameters.Groups.All(x => x.Count >= 2)
             && parameters.Specifications.Count >= 4;
         
-    }
-    
-    private async Task CreateAllTree(List<RoundCreationArgs> rounds) // TODO: научиться получать с фронта нормлаьное дерево, которое можно будет просто создать
-    {
-        var reversedRounds = rounds.OrderByDescending(x => x.Order).ToArray();
-        var finalRound = reversedRounds[0].ToApiModel();
-        finalRound.Id = Guid.NewGuid();
-        await roundsService.AddAsync(finalRound);
-
-        var parentRounds = new Queue<Round>(new[] {finalRound});
-        var createdRounds = 1;
-        var i = 1;
-
-        while (createdRounds < rounds.Count)
-        {
-            var parentRound = parentRounds.Dequeue();
-            var roundsToCreate = reversedRounds.Skip(i).Take(2).ToArray();
-            for (int j = 0; j < 2; j++)
-            {
-                roundsToCreate[j].NextRoundId = parentRound.Id;
-                var apiRound = roundsToCreate[j].ToApiModel();
-                apiRound.Id = Guid.NewGuid();
-                await roundsService.AddAsync(apiRound);
-                parentRounds.Enqueue(apiRound);
-
-                ++createdRounds;
-            }
-
-            i += 2;
-        }
-    }
-
-    private bool IsTreeCorrect(Game game, List<RoundCreationArgs> rounds)
-    {
-        var finalRound = rounds.SingleOrDefault(x => x.NextRoundId == null); // финальный раунд один
-        if (finalRound == null)
-            return false;
-
-        var isTeamsCountCorrect = IsNumberAPowOfTwo(game.Teams.Count); // количество команд это степень двойки
-        if (!isTeamsCountCorrect)
-            return false;
-
-        var isRoundsCountCorrect = IsNumberAPowOfTwo(rounds.Count + 1); // количество раундов это степень двойки, большая нуля
-        if (!isRoundsCountCorrect)
-            return false;
-
-        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-        var allRoundsHaveSpecs = rounds.All(x => x.Specification != null); // У всех раундов есть бизнес сценарии
-        if (!allRoundsHaveSpecs)
-            return false;
-
-        var initialRoundWithTeamsCount = game.Teams.Count / 2;
-        var isStartRoundsHaveTwoTeams = rounds
-            .OrderBy(x => x.Order)
-            .Take(initialRoundWithTeamsCount)
-            .All(x => x.Participants!.Count == 2);
-
-        return isStartRoundsHaveTwoTeams;
     }
 
     private static bool IsNumberAPowOfTwo(int n) => n > 0 && (n & (n - 1)) == 0;
