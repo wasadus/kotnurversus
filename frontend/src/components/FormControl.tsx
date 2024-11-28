c27b6a0737ce3cd22191500d3c93837a9925bfc8
cmbbigis <g0r1lla.disk@gmail.com>
2024-11-28 13:46:56 +0500

    ShowAllPasswordRequirementsAtOnce: требования к паролю показываются всегда

--- frontend/src/components/FormControl.tsx
+++ frontend/src/components/FormControl.tsx
@@ -9,10 +9,11 @@ import { forwardRef } from "react";
 export type FormControlProps = {
   label?: string;
   errorMessage?: string;
+  requirements?: string;
 } & ChakraFormControlProps;
 
 export const FormControl = forwardRef<HTMLDivElement, FormControlProps>(
-  ({ label, errorMessage, children, ...props }, ref) => (
+  ({ label, errorMessage, requirements, children, ...props }, ref) => (
     <ChakraFormControl ref={ref} isInvalid={Boolean(errorMessage)} {...props}>
       {label && (
         <FormLabel
@@ -24,6 +25,16 @@ export const FormControl = forwardRef<HTMLDivElement, FormControlProps>(
         />
       )}
       {children}
+      {!errorMessage && (
+          <FormHelperText
+              mx={1}
+              mt={2}
+              color="grey.500"
+              _dark={{ color: "grey.300" }}
+              fontSize="xs"
+              children={requirements}
+          />
+      )}
       {errorMessage && (
         <FormHelperText
           mx={1}
