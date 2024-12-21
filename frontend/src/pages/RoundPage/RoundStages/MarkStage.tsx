04d8def8282a9f87b9c9cc34abf5ba8b286baf7a
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-21 15:15:42 +0500

    RefactoringForExpert: убрал export default в MinusIcon

--- frontend/src/pages/RoundPage/RoundStages/MarkStage.tsx
+++ frontend/src/pages/RoundPage/RoundStages/MarkStage.tsx
@@ -13,7 +13,7 @@ import api from "~/api";
 import { ButtonWithAlert } from "~/components/ButtonWithAlert";
 import useCustomToast from "~/hooks/useCustomToast";
 import useHandleError from "~/hooks/useHandleError";
-import MinusIcon from "~/icons/MinusIcon";
+import { MinusIcon } from "~/icons/MinusIcon";
 import PlusIcon from "~/icons/PlusIcon";
 import { getErrorApiStatus } from "~/utils/error";
 import queryKeys from "~/utils/query-keys";
