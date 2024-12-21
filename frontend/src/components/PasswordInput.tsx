3231a0305d426f4e9d94c00ab20daae4a3b17db8
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-21 15:13:23 +0500

    RefactoringForExpert: убрал export default в EyeClose

--- frontend/src/components/PasswordInput.tsx
+++ frontend/src/components/PasswordInput.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, useBoolean } from "@chakra-ui/react";
-import EyeClose from "~/icons/EyeClose";
+import { EyeClose } from "~/icons/EyeClose";
 import EyeOpen from "~/icons/EyeOpen";
 import { IconButtonWithTooltip } from "./IconButtonWithTooltip";
 import { Input, InputProps } from "./Input";
