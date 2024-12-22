f04ea8b359f9c2898a3a6a4f4b0a4c080fad10a0
cmbbigis <g0r1lla.disk@gmail.com>
2024-12-22 16:11:11 +0500

    ReconstructionOfTournament: добавил правила для группового этапа

---
+++ backend/Models/Rounds/GroupStageRoundParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace Models.Rounds;
+
+public class GroupStageRoundParameters
+{
+    public int FirstTeamIndex;
+    public int SecondTeamIndex;
+    public int SpecificationIndex;
+
+    public GroupStageRoundParameters(int firstTeamIndex, int secondTeamIndex, int specificationIndex)
+    {
+        FirstTeamIndex = firstTeamIndex;
+        SecondTeamIndex = secondTeamIndex;
+        SpecificationIndex = specificationIndex;
+    }
+}
